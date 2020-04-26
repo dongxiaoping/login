141456e85ff8928ceca7c7a3bc2d99128cd9ad02
dxp <dongxiaoping>
2020-04-26 22:16:21 +0800

    优化数据校验

--- js/loginUtil.js
+++ js/loginUtil.js
@@ -9,26 +9,34 @@
 // +----------------------------------------------------------------------
 
 function checkInData (phone, password) {
-    return true;
+    if(!isPhone (clearBlank (phone))){
+        toastr.error ('电话号码错误！')
+        return false
+    }
+    if (clearBlank (password) == "") {
+        toastr.error ('密码不能为空')
+        return false
+    }
+    return true
 }
 
 function loginReq (phone, password, backFuc) {
     $.ajax ({
         type: "Get",
-        url: LOGIN_IN_REQ+"?phone="+phone+"&password="+password,
+        url: LOGIN_IN_REQ + "?phone=" + phone + "&password=" + password,
         cache: false,
         success: function (data) {
             var return_info = JSON.parse (data);
             if (return_info.status) {
-                backFuc(1, return_info)
+                backFuc (1, return_info)
                 console.log ("登录成功");
             } else {
-                backFuc(0, return_info)
+                backFuc (0, return_info)
                 console.log ("登录失败");
             }
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
-            backFuc(0, '')
+            backFuc (0, '')
             console.log ("上传失败，请检查网络后重试");
         }
     });
--- js/registerUtil.js
+++ js/registerUtil.js
@@ -31,6 +31,22 @@ function postRegister (imgString, nick, phone, password, backFuc) {
 }
 
 function checkInData (iconString, nick, phone, password) {
+    if (iconString.indexOf ('data:image') != 0) {
+        toastr.error ('请上传正确的图像！')
+        return false
+    }
+    if (clearBlank (nick) == "") {
+        toastr.error ('昵称不能为空！')
+        return false
+    }
+    if (!isPhone (clearBlank (phone))) {
+        toastr.error ('电话号码错误！')
+        return false
+    }
+    if (clearBlank (password) == "") {
+        toastr.error ('密码不能为空')
+        return false
+    }
     return true;
 }
 
