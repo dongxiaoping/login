52fe62c7fb6a01eac68cc076d4df5c7c85a78668
dxp <dongxiaoping>
2020-12-30 15:25:28 +0800

    fix

--- js/registerUtil.js
+++ js/registerUtil.js
@@ -28,20 +28,16 @@ function postRegister (imgString, nick, phone, password, code, backFuc) {
 }
 
 function reqSms (phone) {
-    alert('发送')
-/*    $.ajax ({
+    $.ajax ({
         type: "GET",
         url: REQ_SMS,
         data: {phone: phone},
         cache: false,
         success: function (data) {
-            alert('成功')
-            alert(JSON.stringify(data))
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert('失败')
         }
-    });*/
+    });
 }
 
 function checkInData (iconString, nick, phone, password, code) {
